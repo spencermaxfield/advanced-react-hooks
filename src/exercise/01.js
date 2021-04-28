509701b7eab5db423ca20c3ed30811470531a919
Spencer Maxfield <smaxfield@duosecurity.com>
2021-04-28 11:21:33 -0400

    Exercise 1 with extra credits

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,26 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+function countReducer(state, action) {
+  let newState = {...state};
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+  switch (action.type) {
+      case 'INCREMENT':
+        newState.count += action.step;
+        break;
+      default:
+        throw `Unsupported action type: ${action.type}`;
+  }
+
+  return newState;
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount
+  });
+  const increment = () => dispatch({type: 'INCREMENT', step});
+  return <button onClick={increment}>{state.count}</button>
 }
 
 function App() {
